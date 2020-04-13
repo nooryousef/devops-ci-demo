7e4c625c4ff41c59a2637127338b24a21128377e
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-13 18:52:58 +0300

    PWD

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ echo "Building a webpage..."
 
 # Declare and initialize values
 SERVER_NAME=$HOSTNAME
+PWD=$PWD
+
+
+
+    
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
