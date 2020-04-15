d4afbe7154cedc60dc0fc15892098d9590778546
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-15 21:04:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Echo this is the build script
  echo "Building a webpage..."
+ 
 echo  $HOSTNAME
  echo $today
 
@@ -10,11 +11,15 @@ branch=$(git branch | sed -n -e 's /^\* \(.*\)/\1/p')
 # Declare and initialize values
 SERVER=$HOSTNAME
 today=$(date +"%m-%d-%Y")
-
+USR=$USER
   sed "s/{SERVER_NAME}/$SERVER/g"  index.txt
+  sed "s/{USER}/$USR/g" web/index.html
   sed  "s/{TIMESTAMP}/$today/g"  web/index.html 
 
     
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
+echo " I am ${HOSTNAME} ."
+echo " the time is $(date +"%m-%d-%Y") "
+echo " the loged in user is: ${USER} "
