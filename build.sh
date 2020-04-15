5a63ab73c40cb5c54dfd9a646a3c4bafd37d8411
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-15 21:19:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ SERVER=$HOSTNAME
 today=$(date +"%m-%d-%Y")
 USR=$USER
   sed "s/{SERVER_NAME}/ ${HOSTNAME}/g"  web/index.html
-  sed "s/{USER}/$USR/g"  web/index.html
+  sed "s/{USER}/$USR/"  web/index.html
   sed  "s/{TIMESTAMP}/$today/g"  web/index.html 
 
     
