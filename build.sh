c921f73d111d9025bf568a5e0af52c61ef10ca89
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-15 16:24:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 # Echo this is the build script
  echo "Building a webpage..."
  echo  " $HOSTNAME "
- branch=$(git branch | sed -n -e 's /^\* \(.*\)/\1/p')
+HOSTNAME=$fv-az712
+branch=$(git branch | sed -n -e 's /^\* \(.*\)/\1/p')
  
 
 # Declare and initialize values
 SERVER=$HOSTNAME
-  sed 's/{SERVER_NAME}/$SERVER/g ' index.html
+  sed 's/{SERVER_NAME}/$SERVER/g ' web/index.html
 
     
 
