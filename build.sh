9181ae4359133556efa7da1b906841149d1d5e4c
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-15 15:16:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
 # Echo this is the build script
-echo "Building a webpage..."
+ echo "Building a webpage..."
+ 
+  echo "$HOSTNAME"
+ 
 
 # Declare and initialize values
 SERVER_NAME=$HOSTNAME
 
+SERVER=$HOSTNAME
+ NAME=$USER
+  sed "s/{SERVER_NAME}/$SERVER_NAME/g " web/index.txt
 
     
 
