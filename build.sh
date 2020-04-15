0a3e2723ef06b349b6248bbcd3e63d5485169580
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-15 21:08:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ branch=$(git branch | sed -n -e 's /^\* \(.*\)/\1/p')
 SERVER=$HOSTNAME
 today=$(date +"%m-%d-%Y")
 USR=$USER
-  sed "s/{SERVER_NAME}/$SERVER/g"  index.txt
-  sed "s/{USER}/$USR/g"  index.html
-  sed  "s/{TIMESTAMP}/$today/g"  index.html 
+  sed "s/{SERVER_NAME}/$SERVER/g"  web/index.html
+  sed "s/{USER}/$USR/g"  web/index.html
+  sed  "s/{TIMESTAMP}/$today/g"  web/index.html 
 
     
 
