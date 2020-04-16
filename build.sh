cf93e4aa029deb584550bb7f665805bc4c303a12
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-16 09:01:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,9 +12,13 @@ branch=$(git branch | sed -n -e 's /^\* \(.*\)/\1/p')
 SERVER= $(HOSTNAME)
 today=$(date +"%m-%d-%Y")
 USR=$USER
+GIT_REPO=$(devops-ci-demo )
+
+
   sed -i "s/{SERVER_NAME}/${HOSTNAME} /g"  web/index.html
   sed -i "s/{USER}/${USER}/g"  web/index.html
   sed -i "s/{TIMESTAMP}/$today/g"  web/index.html 
+  sed -i "s/{GIT_REPO}/$GIT_REPO/g" web/index.html
 
     
 
