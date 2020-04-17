e440d504781a9e7c9df12b030bf6ab88d58aaaae
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-17 15:59:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ echo "i am $HOSTNAME"
 echo "the time is $(date +%m-%d-%Y)"
 echo "the logged user is $USER"
 
-T=$(date +"%m-%d-%Y")
+TIME=$(date +"%m-%d-%Y")
 g=devops-ci-demo
 DIR=$PWD
-name=$USER
+USR=$USER
 SERVER_NAME=$HOSTNAME
-b=master
+branch=master
 sed -i "s/{SERVER_NAME}/${HOSTNAME}/g" web/index.html
-sed -i "s/{TIMESTAMP}/$T/g" web/index.html
+sed -i "s/{TIMESTAMP}/$TIME/g" web/index.html
 sed -i "s/{GIT_REPO}/$g/g" web/index.html
-sed -i "s/{GIT_BRANCH}/$b/g" web/index.html
+sed -i "s/{GIT_BRANCH}/$branch/g" web/index.html
 sed -i "s~{PWD}~$DIR~g" web/index.html
-sed -i "s/{USER}/$name/g" web/index.html
+sed -i "s/{USER}/$USR/g" web/index.html
 sed -i "s/{AZURE_VARIABLE}/${AZURE_VARIABLE}/g" web/index.html
