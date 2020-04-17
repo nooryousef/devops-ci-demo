76c0d109dc05215844077c85c25a28cd25ae980e
nooryousef <62786568+nooryousef@users.noreply.github.com>
2020-04-17 15:57:14 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,36 +1,22 @@
+
+
 #! /bin/bash
+#print this line
+echo "running this build script"
+echo "i am $HOSTNAME"
+echo "the time is $(date +%m-%d-%Y)"
+echo "the logged user is $USER"
 
-# Echo this is the build script
- echo "Building a webpage..."
- 
-echo  $HOSTNAME
- echo $today
- echo $REPO
-
-
-# Declare and initialize values
-SERVER= $(HOSTNAME)
-today=$(date +"%m-%d-%Y")
-USR=$USER
- PWD="$(pwd)
-
-  sed -i "s/{SERVER_NAME}/${HOSTNAME} /g"  web/index.html
-  sed -i "s/{USER}/${USER}/g"  web/index.html
-  sed -i "s/{TIMESTAMP}/$today/g"  web/index.html 
-  sed -i "s/{GIT_REPO}/${devops-ci-demo}/g"   web/index.html
-
-   sed -i "s/${PWD}/$PWD/g" web/index.html
-   sed -i "s/{AZURE_VARIABLE}/${AZURE_VARIABLE}/g" web/index.html
-
-   
-   
-   
-   
-
-    
-
-# Replace values in index.html
-## TODO: Replace {SEVER_NAME} with $SEVER_NAME
-echo " I am ${HOSTNAME} ."
-echo "  $(date +"%m-%d-%Y") "
-echo " the loged  user is: ${USER} "
+T=$(date +"%m-%d-%Y")
+g=devops-ci-demo
+DIR=$PWD
+name=$USER
+SERVER_NAME=$HOSTNAME
+b=master
+sed -i "s/{SERVER_NAME}/${HOSTNAME}/g" web/index.html
+sed -i "s/{TIMESTAMP}/$T/g" web/index.html
+sed -i "s/{GIT_REPO}/$g/g" web/index.html
+sed -i "s/{GIT_BRANCH}/$b/g" web/index.html
+sed -i "s~{PWD}~$DIR~g" web/index.html
+sed -i "s/{USER}/$name/g" web/index.html
+sed -i "s/{AZURE_VARIABLE}/${AZURE_VARIABLE}/g" web/index.html
